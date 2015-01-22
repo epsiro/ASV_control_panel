69c5901fc739f318b6231c3642f83e2035230ab1
Bendik S. Søvegjarto <bendik@sovegjarto.com>
2015-01-22 20:28:42 +0100

    possible to use some offline tiles

---
+++ public/img/tiles/wget_script.sh
@@ -0,0 +1,41 @@
+mkdir 69435
+cd 69435
+wget https://a.tiles.mapbox.com/v3/examples.map-i87786ca/17/69435/38108.png
+wget https://a.tiles.mapbox.com/v3/examples.map-i87786ca/17/69435/38109.png
+wget https://a.tiles.mapbox.com/v3/examples.map-i87786ca/17/69435/38110.png
+cd ..
+
+mkdir 69436
+cd 69436
+wget https://a.tiles.mapbox.com/v3/examples.map-i87786ca/17/69436/38108.png
+wget https://b.tiles.mapbox.com/v3/examples.map-i87786ca/17/69436/38109.png
+wget https://b.tiles.mapbox.com/v3/examples.map-i87786ca/17/69436/38110.png
+cd ..
+
+mkdir 69437
+cd 69437
+wget https://b.tiles.mapbox.com/v3/examples.map-i87786ca/17/69437/38108.png
+wget https://c.tiles.mapbox.com/v3/examples.map-i87786ca/17/69437/38109.png
+wget https://a.tiles.mapbox.com/v3/examples.map-i87786ca/17/69437/38110.png
+cd ..
+
+mkdir 69438
+cd 69438
+wget https://c.tiles.mapbox.com/v3/examples.map-i87786ca/17/69438/38108.png
+wget https://a.tiles.mapbox.com/v3/examples.map-i87786ca/17/69438/38109.png
+wget https://b.tiles.mapbox.com/v3/examples.map-i87786ca/17/69438/38110.png
+cd ..
+
+mkdir 69439
+cd 69439
+wget https://a.tiles.mapbox.com/v3/examples.map-i87786ca/17/69439/38108.png
+wget https://b.tiles.mapbox.com/v3/examples.map-i87786ca/17/69439/38109.png
+wget https://c.tiles.mapbox.com/v3/examples.map-i87786ca/17/69439/38110.png
+cd ..
+
+mkdir 69440
+cd 69440
+wget https://b.tiles.mapbox.com/v3/examples.map-i87786ca/17/69440/38108.png
+wget https://b.tiles.mapbox.com/v3/examples.map-i87786ca/17/69440/38109.png
+wget https://a.tiles.mapbox.com/v3/examples.map-i87786ca/17/69440/38110.png
+cd ..
